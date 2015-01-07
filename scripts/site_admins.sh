35b0ace0b9c79123e0dd67cf699ac20d9dd845bb
Michal Prochazka <michalp@ics.muni.cz>
2015-01-07 20:22:50 +0100

    Curl used instead of wget.

--- scripts/site_admins.sh
+++ scripts/site_admins.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Get list of site security officers
-wget -q -O /tmp/pakiti-seo.xml --no-check-certificate --certificate=/etc/apache2/ssl/2014/pakiti.egi.eu-2014.pem --private-key=/etc/apache2/ssl/2014/pakiti.egi.eu-2014.key "https://goc.egi.eu/gocdbpi/private/?method=get_site_contacts&roletype=Site%20Security%20Officer"
+curl -s --capath /etc/grid-security/certificates/ --cert /etc/apache2/ssl/pakiti.egi.eu.pem --key /etc/apache2/ssl/pakiti.egi.eu.key "https://goc.egi.eu/gocdbpi/private/?method=get_site_contacts&roletype=Site%20Security%20Officer" -o /tmp/pakiti-seo.xml
 
 # Get site security info, it CSIRT email
-wget -q -O /tmp/pakiti-csirt.xml --no-check-certificate --certificate=/etc/apache2/ssl/pakiti.egi.eu.pem --private-key=/etc/apache2/ssl/pakiti.egi.eu.key "https://goc.egi.eu/gocdbpi/private/?method=get_site_security_info"
+curl -s --capath /etc/grid-security/certificates/ --cert /etc/apache2/ssl/pakiti.egi.eu.pem --key /etc/apache2/ssl/pakiti.egi.eu.key "https://goc.egi.eu/gocdbpi/private/?method=get_site_security_info" -o /tmp/pakiti-csirt.xml
 
 # Get list of all NGI contacts
-wget -q -O /tmp/pakiti-ngi-so.xml --no-check-certificate --certificate=/etc/apache2/ssl/pakiti.egi.eu.pem --private-key=/etc/apache2/ssl/pakiti.egi.eu.key "https://goc.egi.eu/gocdbpi/private/?method=get_roc_contacts"
+curl -s --capath /etc/grid-security/certificates/ --cert /etc/apache2/ssl/pakiti.egi.eu.pem --key /etc/apache2/ssl/pakiti.egi.eu.key "https://goc.egi.eu/gocdbpi/private/?method=get_roc_contacts" -o /tmp/pakiti-ngi-so.xml
 
 # Get list of sites
-wget -q -O /tmp/pakiti-sites.xml --no-check-certificate --certificate=/etc/apache2/ssl/pakiti.egi.eu.pem --private-key=/etc/apache2/ssl/pakiti.egi.eu.key "https://goc.egi.eu/gocdbpi/private/?method=get_site_list&certification_status=Certified&production_status=Production"
+curl -s --capath /etc/grid-security/certificates/ --cert /etc/apache2/ssl/pakiti.egi.eu.pem --key /etc/apache2/ssl/pakiti.egi.eu.key "https://goc.egi.eu/gocdbpi/private/?method=get_site_list&certification_status=Certified&production_status=Production" -o /tmp/pakiti-sites.xml
 
 cd /var/www/pakiti-egi/scripts && /usr/bin/php site_admins.php 2>/dev/null
 
