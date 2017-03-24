08c0490a4f661d294d0745e48481dfe42f2e5ff2
Michal Prochazka <michalp@ics.muni.cz>
2017-03-24 12:37:39 +0100

    Use TLS when talking to the LDAP server.

--- scripts/admins.sh
+++ scripts/admins.sh
@@ -8,7 +8,7 @@ echo "<?php" > $TMP
 echo "\$admin_dns = array (" >> $TMP
 cat $BASE | grep -v "^[ \t\n]*#" >> $TMP
 
-for i in `ldapsearch -LLL -x -H ldap://aldor.ics.muni.cz/  -b 'cn=csirt,ou=groups,dc=egi,dc=eu' member|grep -v '^dn' |sed -e 's/member: //'`; do ldapsearch -LLL -x -H ldap://aldor.ics.muni.cz/  -b "$i" userCertificateSubject | perl -p -0040 -e 's/\n //' |  grep userCertificateSubject | sed -e 's/^userCertificateSubject: \(.*\)/"\1",/'; done >> $TMP
+for i in `ldapsearch -LLL -x -H ldaps://aldor.ics.muni.cz/  -b 'cn=csirt,ou=groups,dc=egi,dc=eu' member|grep -v '^dn' |sed -e 's/member: //'`; do ldapsearch -LLL -x -H ldaps://aldor.ics.muni.cz/  -b "$i" userCertificateSubject | perl -p -0040 -e 's/\n //' |  grep userCertificateSubject | sed -e 's/^userCertificateSubject: \(.*\)/"\1",/'; done >> $TMP
 
 echo "); ?>" >> $TMP
 
